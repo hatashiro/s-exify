5ca2e9416c356314102d726f24362aca5ccd9b57
Hyunje Jun <me@noraesae.net>
2018-11-16 14:41:29 +0900

    Implement main functionality

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,5 +1,72 @@
-import { init } from "../";
+import { WAT, parse, beautify } from "../";
 
-test("init", () => {
-  expect(init).toBe("hello, s-expression");
+test("parse", () => {
+  const parseTest = (str: string, expected: WAT) =>
+    expect(parse(str)).toEqual(expected);
+
+  parseTest("()", []);
+  parseTest("(i32.const 1234)", ["i32.const", "1234"]);
+  parseTest("(i32\\ const 1234)", ["i32\\ const", "1234"]);
+  parseTest(`(data "hello, world!")`, ["data", `"hello, world!"`]);
+  parseTest(
+    `
+    (module
+      (global $g (import "js" "global") (mut i32))
+      (func (export "getGlobal") (result i32)
+            (get_global $g))
+      (func (export "incGlobal")
+            (set_global $g
+                (i32.add (get_global $g) (i32.const 1))))
+    )
+  `,
+    [
+      "module",
+      ["global", "$g", ["import", `"js"`, `"global"`], ["mut", "i32"]],
+      [
+        "func",
+        ["export", `"getGlobal"`],
+        ["result", "i32"],
+        ["get_global", "$g"]
+      ],
+      [
+        "func",
+        ["export", `"incGlobal"`],
+        [
+          "set_global",
+          "$g",
+          ["i32.add", ["get_global", "$g"], ["i32.const", "1"]]
+        ]
+      ]
+    ]
+  );
+});
+
+test("beautify", () => {
+  const beautifyTest = (str: string, expected: string) =>
+    expect(beautify(str)).toEqual(expected);
+
+  beautifyTest("()", "()");
+  beautifyTest("(i32.const 1234)", "(i32.const 1234)");
+  beautifyTest("(i32\\ const 1234)", "(i32\\ const 1234)");
+  beautifyTest(`(data "hello, world!")`, `(data "hello, world!")`);
+  beautifyTest(
+    `
+    (module
+      (global $g (import "js" "global") (mut i32))
+      (func (export "getGlobal") (result i32)
+            (get_global $g))
+      (func (export "incGlobal")
+            (set_global $g
+                (i32.add (get_global $g) (i32.const 1))))
+    )
+  `,
+    `(module
+  (global $g (import "js" "global") (mut i32))
+  (func (export "getGlobal") (result i32) (get_global $g))
+  (func
+    (export "incGlobal")
+    (set_global $g (i32.add (get_global $g) (i32.const 1)))
+  )
+)`
+  );
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,97 @@
-export const init = "hello, s-expression";
+export interface WAT extends Array<string | WAT> {}
+
+export function parse(input: string | Array<string>): WAT {
+  // convert input to an array of characters
+  input = typeof input === "string" ? Array.from(input) : input;
+
+  while (input[0].match(/\s/)) input.shift(); // skip whitespaces
+  if (input[0] === "(") {
+    // drop '('
+    input.shift();
+  } else {
+    throw new Error(
+      `Input is not valid: unexpected '${input[0]}' at the beginning`
+    );
+  }
+
+  const result: WAT = [];
+  let node = "";
+
+  while (true) {
+    let c = input.shift();
+
+    if (!c) {
+      if (!node) {
+        break;
+      } else {
+        throw new Error(`Input is not valid: unexpected '${node}' at the end`);
+      }
+    }
+
+    if (c === ")") {
+      if (node) result.push(node);
+      break;
+    } else if (c === "\\") {
+      c += input.shift();
+      node += c;
+    } else if (c.match(/\s/)) {
+      if (node) result.push(node);
+      node = "";
+    } else if (c === "(") {
+      input.unshift(c);
+      result.push(parse(input));
+    } else if (c === '"') {
+      node += c;
+      // parse string
+      while ((c = input.shift())) {
+        // skip \"
+        if (c === "\\" && input[0] === '"') {
+          c += input.shift();
+        }
+        node += c;
+        if (c === '"') break;
+      }
+    } else {
+      node += c;
+    }
+  }
+
+  return result;
+}
+
+function length(wat: WAT): number {
+  return wat.reduce(
+    (res, node) =>
+      res + (typeof node === "string" ? node.length : length(node)),
+    0
+  );
+}
+
+function indent(str: string): string {
+  return str
+    .split("\n")
+    .map(s => "  " + s)
+    .join("\n");
+}
+
+function stringify(node: string | WAT) {
+  return typeof node === "string" ? node : beautify(node);
+}
+
+export function beautify(wat: string | WAT): string {
+  wat = typeof wat === "string" ? parse(wat) : wat;
+
+  if (length(wat) < 50) {
+    return `(${wat.map(stringify).join(" ")})`;
+  } else {
+    return (
+      `(${wat[0]}\n` +
+      wat
+        .slice(1)
+        .map(stringify)
+        .map(indent)
+        .join("\n") +
+      "\n)"
+    );
+  }
+}
