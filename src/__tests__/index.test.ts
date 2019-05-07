a2da8400d73504c9f3334e12fba92d0d263fc6df
Hyunje Jun <me@noraesae.net>
2019-05-07 19:15:36 +0900

    Fix and archive test fixtures

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -68,13 +68,28 @@ test("beautify", () => {
     )
   `,
     `(module
-  (global $g (import "js" "global") (mut i32))
-  (func (export "getGlobal") (result i32) (get_global $g))
+  (global
+    $g
+    (import "js" "global")
+    (mut i32)
+  )
+  (func
+    (export "getGlobal")
+    (result i32)
+    (get_global $g)
+  )
   (func
     (export "incGlobal")
-    (set_global $g (i32.add (get_global $g) (i32.const 1)))
+    (set_global
+      $g
+      (i32.add
+        (get_global $g)
+        (i32.const 1)
+      )
+    )
   )
 )`
   );
   beautifyTest(fixture("hanoi.wat"), fixture("hanoi.out.wat"));
+  beautifyTest(fixture("OptimTease.wat"), fixture("OptimTease.out.wat"));
 });
